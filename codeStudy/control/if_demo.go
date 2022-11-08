cd3047a58099ab023a7053dc0013ece179f63914
baimiyishu13 <baimyishu13@163.com>
2022-11-08 22:51:06 +0800

    if 语句

--- codeStudy/control/if_demo.go
+++ codeStudy/control/if_demo.go
@@ -3,5 +3,14 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("a")
+
+	var a int = 15
+
+	//if 如果 条件 { } 条件不满足 if else
+	if a > 20 {
+		fmt.Println("a > 20")
+	}
+	if a > 10 {
+		fmt.Println("a > 20")
+	}
 }
