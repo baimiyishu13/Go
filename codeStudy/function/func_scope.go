7a48ddd6d1009285b331d955a9496026fb1ba654
baimiyishu13 <baimyishu13@163.com>
2022-11-12 23:23:59 +0800

    变量在函数中的作用域：都遵循就近原则！

--- codeStudy/function/func_scope.go
+++ codeStudy/function/func_scope.go
@@ -1,5 +1,35 @@
 package main
 
-func main() {
+import "fmt"
 
+// 全局变量
+var num int = 100
+
+func main() {
+	// 函数体内的局部变量
+	temp := 100
+	// if、for语句定义的一次性变量局部变量
+	if b := 1; b <= 10 {
+		// 语句内的局部变量
+		temp := 50
+		fmt.Println(temp) // 局部变量，就近原则
+		fmt.Println(b)
+	}
+	fmt.Println(temp)
+	fmt.Println(num)
+
+	f1()
+	f2()
+}
+
+// 内部
+func f1() {
+	a := 1
+	fmt.Println(a)
+	num := 30 // 就近原则
+	fmt.Println(num)
+}
+func f2() {
+	//fmt.Println(a)	// 不能使用其他函数定义的变量
+	fmt.Println(num)
 }
